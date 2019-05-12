39c003d0b530e4211608cab38a454b0f26fe2f08
henrymgalvan <hgalvan1971@yahoo.com>
2019-05-12 21:45:01 +0800

    --- Auto-Git Commit ---

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from "@angular/core";
-import { MatButtonModule, MatIconModule } from '@angular/material';
+import { MatButtonModule, MatIconModule } from "@angular/material";
 @NgModule({
     imports: [MatButtonModule, MatIconModule],
     exports: [MatButtonModule, MatIconModule]
