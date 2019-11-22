bb3f66232a50bf73c9233edc548fe44bafea6626
henrymgalvan <hgalvan1971@yahoo.com>
2019-11-22 20:16:48 +0800

    --- Auto-Git Commit ---

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,8 @@ import { HeaderComponent } from './navigation/header/header.component';
 import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 import { StopTrainingComponent } from './training/current-training/stop-training.component';
 import { AuthService } from './auth/auth.service';
+import { TrainingService } from './training/training.service';
+
 
 @NgModule({
   declarations: [
@@ -42,8 +44,8 @@ import { AuthService } from './auth/auth.service';
     FlexLayoutModule,
     FormsModule
   ],
-  providers: [AuthService],
-  bootstrap: [AppComponent],
-  entryComponents:[StopTrainingComponent]
+  providers: [ AuthService, TrainingService ],
+  bootstrap: [ AppComponent ],
+  entryComponents: [ StopTrainingComponent ]
 })
 export class AppModule { }
--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -7,7 +7,7 @@ import { AuthService } from './auth.service';
 export class AuthGuard implements CanActivate {
     constructor(private authService: AuthService, private router: Router) {}
 
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot){
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
         if (this.authService.isAuth()) { return true; } else { this.router.navigate(['/login']); }
     }
 }
