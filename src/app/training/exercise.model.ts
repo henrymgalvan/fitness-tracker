738c25f81d98656b2ac8ac26f6dbf8cde2cad4f6
henrymgalvan <hgalvan1971@yahoo.com>
2019-11-23 12:08:05 +0800

    Lesson 60 Handling complete and cancel events.

--- src/app/training/exercise.model.ts
+++ src/app/training/exercise.model.ts
@@ -4,5 +4,5 @@ export interface Exercise {
     duration: number;
     calories: number;
     date?: Date;
-    state?: 'completed' | 'canceleed' | null;
+    state?: 'completed' | 'cancelled' | null;
 }
--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -11,6 +11,7 @@ export class TrainingService {
     ];
 
     private runningExercise: Exercise;
+    private exercises: Exercise[] = [];
 
     getAvailableExercises() {
         return this.availableExercises.slice();
@@ -19,6 +20,28 @@ export class TrainingService {
     startExercise(selectedId: string) {
         const runningExercise = this.availableExercises.find(ex => ex.id === selectedId);
         this.exerciseChanged.next({ ...this.runningExercise });
+    }
+
+    completeExercise() {
+        this.exercises.push({
+            ...this.runningExercise,
+            date: new Date(),
+            state: 'completed'
+        });
+        this.runningExercise = null;
+        this.exerciseChanged.next(null);
+    }
+
+    cancelExercise(progress: number) {
+        this.exercises.push({
+            ...this.runningExercise,
+            duration: this.runningExercise.duration * (progress / 100),
+            calories: this.runningExercise.duration * (progress / 100),
+            date: new Date(),
+            state: 'cancelled'
+        });
+        this.runningExercise = null;
+        this.exerciseChanged.next(null);
     }
 
     getRunningExercise() {
