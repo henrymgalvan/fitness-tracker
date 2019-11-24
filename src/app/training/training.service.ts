3d007e2b347a60b88f491c3cb40a385d97e8bcac
henrymgalvan <hgalvan1971@yahoo.com>
2019-11-24 20:38:07 +0800

    --- Auto-Git Commit ---

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -22,6 +22,19 @@ export class TrainingService {
         this.exerciseChanged.next({ ...this.runningExercise });
     }
 
+    // completeExercise() {
+    //     this.exercises.push({
+    //         id: this.runningExercise.id,
+    //         name: this.runningExercise.name,
+    //         duration: this.runningExercise.duration,
+    //         calories: this.runningExercise.calories,
+    //         date: new Date(),
+    //         state: 'completed'
+    //     });
+    //     this.runningExercise = null;
+    //     this.exerciseChanged.next(null);
+    // }
+
     completeExercise() {
         this.exercises.push({
             ...this.runningExercise,
